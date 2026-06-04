6e06c1fc5001f54dd1734c0770b26a51a481ef09
aqe-gl <filatovnick11@gmail.com>
2026-06-04 20:01:58 +0100

    created inputs for numbers, added operator buttons

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ document.getElementById("enter-name").onclick = function () {
     let ans = prompt("Enter your name: ");
     console.log(ans);
 }
+
+document.querySelector('.add').onclick = function () {
+    //alert(2+2)
+    //console.log(document.querySelector(".number1").value)
+    //console.log(document.querySelector(".number2").value)
+    let num1 = document.querySelector('.number1').value;
+    let num2 = document.querySelector('.number2').value
+    let ans = parseInt(num1) + parseInt(num2)
+    //console.log(typeof(num1));
+    alert(ans);
+}
+
+document.querySelector('.subtract').onclick = function () {
+    let num1 = document.querySelector('.number1').value;
+    let num2 = document.querySelector('.number2').value;
+    let ans = parseInt(num1) - parseInt(num2);
+    alert(ans);
+}
